2cab6e7702ccd479faa5a3983ef80feee2081fb8
Bandar-Zuhair <my.github.websites@gmail.com>
2024-12-27 17:54:44 +0700

    Update holidays-island-travel.js

--- holidays-island-travel.js
+++ holidays-island-travel.js
@@ -488,7 +488,7 @@ function openFullScreenImage(src, text) {
     const whatsappButton = document.createElement('a');
     whatsappButton.className = 'whatsapp_button';
     whatsappButton.innerText = 'إرسال هذا العرض';
-    whatsappButton.href = `https://wa.me/+966560378994?text=طلب%20حجز%20هذا%20العرض:%0Ahttps://mohammed-website.github.io/holidaysislandtravel/${encodeURIComponent(src)}`;
+    whatsappButton.href = `https://wa.me/+966548098670?text=طلب%20حجز%20هذا%20العرض:%0Ahttps://mohammed-website.github.io/holidaysislandtravel/${encodeURIComponent(src)}`;
     fullScreenDiv.appendChild(whatsappButton);
 
     // Close on background click
@@ -729,7 +729,7 @@ openWhatsAppNumber = function () {
 
     insertNewClick('alseef.com');
 
-    const whatsappNumber = "+966560378994";
+    const whatsappNumber = "+966548098670";
     const message = encodeURIComponent('سلام عليكم ورحمة الله وبركاته'); // Optional pre-filled message
     const url = `https://wa.me/${whatsappNumber}?text=${message}`;
     window.open(url, "_blank"); // Opens in a new tab
